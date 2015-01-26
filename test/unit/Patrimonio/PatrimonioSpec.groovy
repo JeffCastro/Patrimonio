69daa7e70b0a9fd8729776042d5d48531c54e279
JeffCastro <jefferson.castro@sct.ce.gov.br>
2015-01-26 16:33:00 -0300

    teste

---
+++ test/unit/Patrimonio/PatrimonioSpec.groovy
@@ -0,0 +1,20 @@
+package Patrimonio
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Patrimonio)
+class PatrimonioSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
